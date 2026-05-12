fbc5811820724ae60985638c2dd779b75ce31879
Moo-Ood <little.shark.pro@gmail.com>
2026-05-13 01:12:57 +0700

    update cat to new one

--- hello.py
+++ hello.py
@@ -1,11 +1,34 @@
-print('Hello World')
-text = '''
-            /\_____/\\
-           /  o   o  \\
-          ( ==  ^  == )
-           )         (
-          (           )
-         ( (  )   (  ) )
-        (__(__)___(__)__)
-'''
-print(text)
+from datetime import datetime
+import os
+
+# ANSI Colors
+GREEN = "\033[92m"
+CYAN = "\033[96m"
+YELLOW = "\033[93m"
+RESET = "\033[0m"
+
+os.system("")  # Enable ANSI on some Windows terminals
+
+cat = rf"""
+{CYAN}
+ /\_/\\
+( o.o )
+ > ^ <
+{RESET}
+"""
+
+banner = f"""
+{GREEN}======================================
+        PYTHON HELLO PROGRAM
+======================================{RESET}
+"""
+
+message = f"""
+{YELLOW}Hello, World! 👋
+Current Time: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+{RESET}
+"""
+
+print(banner)
+print(cat)
+print(message)
